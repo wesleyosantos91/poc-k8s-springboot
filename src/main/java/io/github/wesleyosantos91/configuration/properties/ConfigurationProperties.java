607cba0407203f64fb181f06dd1db7fb244555bc
Wesley Oliveira Santos <wesleyosantos91@gmail.com>
2023-05-07 22:10:24 -0300

    feat: configurado kafka e mysql como clusterip

--- src/main/java/io/github/wesleyosantos91/configuration/properties/ConfigurationProperties.java
+++ src/main/java/io/github/wesleyosantos91/configuration/properties/ConfigurationProperties.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties
 public class ConfigurationProperties {
 
-
     @Value("${app.metrics.timeout}")
     private Integer timeout;
 }
